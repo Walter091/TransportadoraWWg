fbc4d8adbda6e9b7cb02bdce1e084cdc8632e24c
willamesJS <willamesantos626@gmail.com>
2022-09-16 10:02:58 -0300

    add classe de retorno para requisições

--- src/main/java/br/com/gw_sistemas/transportadora_wwg/controller/IndexController.java
+++ src/main/java/br/com/gw_sistemas/transportadora_wwg/controller/IndexController.java
@@ -2,24 +2,21 @@ package br.com.gw_sistemas.transportadora_wwg.controller;
 
 import br.com.gw_sistemas.transportadora_wwg.service.ServiceLogin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 //"/Transportadora"
+@Controller
 @RestController()
 public class IndexController {
 
     @Autowired
     private ServiceLogin serviceLogin;
 
-    @GetMapping("/")
+    @GetMapping(value = {"/", "/Login"})
     public ModelAndView index() {
-        return login();
-    }
-
-    @GetMapping("/Login")
-    public ModelAndView login() {    
         return new ModelAndView("Login");
     }
 
--- src/main/java/br/com/gw_sistemas/transportadora_wwg/controller/PessoaController.java
+++ src/main/java/br/com/gw_sistemas/transportadora_wwg/controller/PessoaController.java
@@ -3,6 +3,7 @@ package br.com.gw_sistemas.transportadora_wwg.controller;
 import br.com.gw_sistemas.transportadora_wwg.model.Pessoa;
 import br.com.gw_sistemas.transportadora_wwg.service.ServicePessoa;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/Transportadora/Opcoes")
+@Controller
+@RestController("/Transportadora")
 public class PessoaController {
 
     @Autowired
